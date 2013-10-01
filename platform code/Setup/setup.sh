173bd55556d255cae3c9ddc3739c632f24841198
sam-b <samdanielbrown@gmail.com>
2013-10-01 23:16:24 +0100

    began setting up web.py to replace cgi scripts

--- platform code/Setup/setup.sh
+++ platform code/Setup/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #install dependencies needed for processor node
-sudo apt-get install python sqlite git
+sudo apt-get install python sqlite git python-pip
 git clone git://github.com/smarnach/pyexiftool.git
 cd pyexiftool
 sudo python setup.py install
+sudo pip install web.py
