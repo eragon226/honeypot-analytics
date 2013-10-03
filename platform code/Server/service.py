df18ee32e824ae24f2fef7fa8d0916e51b57c9f3
sam-b <samdanielbrown@gmail.com>
2013-10-03 23:33:02 +0100

    web.py basic setup finished cgi stuff gone

--- platform code/Server/service.py
+++ platform code/Server/service.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
 import web
 import database
 import os
 import hashlib
 import sys
+import exiftool
 
 urls = (
 	'/', 'Index',
@@ -56,6 +58,7 @@ class Sample:
 	def POST(self):
 		x = web.input(myfile={})
 		filedir = 'uploads/samples' # change this to the directory you want to store the file in.
+		con = database.connect()
 		if 'myfile' in x: # to check if the file-object is created
 			filename=hashlib.md5(x.myfile.filename).hexdigest()
 			if fileExists(filedir + "/" + filename):
@@ -64,7 +67,16 @@ class Sample:
 				fout = open(filedir +'/'+ filename,'wb') # creates the file where the uploaded file should be stored
 				fout.write(x.myfile.file.read()) # writes the uploaded file to the newly created file.
 				fout.close() # closes the file, upload complete.
-		raise web.seeother('/upload/sample')
+				with exiftool.ExifTool() as et:
+					metadata = et.get_metadata(filedir + '/' + filename)
+					web.debug(metadata)
+					entry = (filename,
+						metadata['File:FileSize'],
+						metadata['File:FileModifyDate'],
+						metadata['File:FileType'],
+						metadata['File:MIMEType'])
+					database.add(con,entry)
+			raise web.seeother('/upload/sample')
 
 
 def fileExists(fullPath):
