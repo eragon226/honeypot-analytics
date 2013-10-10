28b5fa52c8d8aa0b6c96c490d9cd79b9cd6b7825
sam-b <samdanielbrown@gmail.com>
2013-10-10 23:22:31 +0100

    dionaea now has a cron job for sending logs in its setup script

--- install scripts/dionaea-setup.sh
+++ install scripts/dionaea-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#dionaea install script - run as ./dionaea-setup.sh "private key" "user@host"
+#dionaea install script - run as ./dionaea-setup.sh "private key" "user@host" "address of platform post log url"
 
 SCRIPT="#install dionaea and p0f
 sudo add-apt-repository ppa:honeynet/nightly
@@ -21,6 +21,12 @@ sudo chown -R nobody:nogroup /var/dionaea/
 sudo mv /etc/dionaea/dionaea.conf.dist /etc/dionaea/dionaea.conf
 sudo sed -i 's/var\/dionaea\///g' /etc/dionaea/dionaea.conf
 sudo sed -i 's/log\//\/var\/dionaea\/log\//g' /etc/dionaea/dionaea.conf
+
+#echo new cron into cron file
+echo '@daily bash curl --form \"myfile=@/var/dionaea/logsql.sqlite\" $3 ' >> mycron
+#install new cron file
+crontab mycron
+rm mycron
 
 #start the honeypot
 sudo dionaea -c /etc/dionaea/dionaea.conf -w /var/dionaea -u nobody -g nogroup -D"
